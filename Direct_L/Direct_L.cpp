5f353522117f21c9384aaf0fb49354096a0158c8
qwq2018ad <neil910119@gmail.com>
2024-09-17 14:37:21 +0800

    新增 s1、s2...的子字串比對，原本只有s1

--- Direct_L/Direct_L.cpp
+++ Direct_L/Direct_L.cpp
@@ -19,26 +19,28 @@ int hammingDistance(const string& str1, const string& str2) {
 }
 
 vector<int> SlidingWindow(const string& T, int l) {
-    string p =T.substr(0, l);
-    int Tsize=T.length();
+    int Tsize = T.length();
     std::vector<int> result;
-    cout << Tsize - l + 1 << endl;
-    for (int i = 0; i < Tsize - l + 1; i++) {
-        string s = T.substr(i, l);
-        cout <<i<<" :" << s << endl;
-        int distance = 0;
-        for (int j = 0; j < l; j++) {
-            distance += (p[j] != s[j]) ? 1 : 0;
+    //for (int k = 0; k < Tsize - l; k++) {//every substring
+        string p = T.substr(0, l);//k or 0
+        cout << p << endl;
+        for (int i = 0; i < Tsize - l + 1; i++) {//k or 0
+            string s = T.substr(i, l);
+            cout << i << " :" << s << endl;
+            int distance = 0;
+            for (int j = 0; j < l; j++) {
+                distance += (p[j] != s[j]) ? 1 : 0;
+            }
+            result.push_back(distance);
         }
-        result.push_back(distance);
-    }
+    //}
     return result;
 }
 
 int main() {
-    int l = 4; // 8, 16, 32, 64
+    int l = 4; //4, 8, 16, 32, 64
 
-    //string filename = "dna_1G.txt"; // 檔案名稱
+    //string filename = "dna_500M.txt"; // 檔案名稱
     //ifstream infile(filename);       // 以讀取模式打開檔案
 
     //// 檢查檔案是否成功打開
@@ -50,36 +52,54 @@ int main() {
     //stringstream buffer;
     //buffer << infile.rdbuf();  // 將檔案內容讀入到 stringstream 中
 
-    //string atcgStr1 = buffer.str();  // 將 stringstream 中的內容轉換為 string
-    //string atcgStr2 = atcgStr1;
+    //string T = buffer.str();  // 將 stringstream 中的內容轉換為 string
+    //string atcgStr2 = T;
 
     string T = "GAGTCAGAGTA";
-
+    int Tsize = T.length();
     double at = 0;
     int distance = 0;
     std::vector<int> result;
     for (int j = 0; j < 1; j++) {
         // 開始計時
         auto start = chrono::high_resolution_clock::now();
-
         // 計算漢明距離
         result = SlidingWindow(T,l);
-
         // 結束計時
         auto end = chrono::high_resolution_clock::now();
-
         // 計算經過的時間
         chrono::duration<double> duration = end - start;
-        
         // 輸出結果
         /*cout << "漢明距離: " << distance << endl;
         cout << duration.count() << endl;*/
         at += duration.count();
 
     }
-    for (int i = 0; i < result.size(); ++i) {
+    //cout << " T : " << T << endl;
+
+    ////print 每個L-substring
+    //int count = Tsize - l + 1;  // 初始換行間隔為8
+    //int index = 0;  // 用來跟蹤元素位置
+    //while (index < result.size()) {
+    //    // 打印 `count` 個元素
+    //    for (int i = 0; i < count && index < result.size(); ++i) {
+    //        std::cout << result[index] << " ";
+    //        ++index;
+    //    }
+
+    //    std::cout << std::endl;  // 換行
+
+    //    // 下一次的換行間隔遞減
+    //    if (count > 1) {
+    //        --count;
+    //    }
+    //}
+
+    for (int i = 0; i < result.size(); ++i) {// one substring
         cout << result[i] << " ";
     }
-    //cout << "長度為 " << atcgStr1.length() << " time : " << at << endl;
+
+    cout << endl;
+    cout << "長度為 " << T.length()<< " L=" << l<< " time : " << at << endl;
     return 0;
 }
