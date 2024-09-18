c1cf5fd803dc4ee6f6818cf0ffce4e1e2a4ecb71
qwq2018ad <neil910119@gmail.com>
2024-09-18 23:38:42 +0800

    vector to array

--- Direct_L/Direct_L.cpp
+++ Direct_L/Direct_L.cpp
@@ -18,53 +18,60 @@ int hammingDistance(const string& str1, const string& str2) {
     return distance;
 }
 
-vector<int> SlidingWindow(const string& T, int l) {
+void SlidingWindow(const string& T,int *result, int l) {
     int Tsize = T.length();
-    std::vector<int> result;
     //for (int k = 0; k < Tsize - l; k++) {//every substring
         string p = T.substr(0, l);//k or 0
-        cout << p << endl;
+        //cout << p << endl;
         for (int i = 0; i < Tsize - l + 1; i++) {//k or 0
             string s = T.substr(i, l);
-            cout << i << " :" << s << endl;
+            //cout << i << " :" << s << endl;
             int distance = 0;
             for (int j = 0; j < l; j++) {
                 distance += (p[j] != s[j]) ? 1 : 0;
             }
-            result.push_back(distance);
+            result[i]=distance;
         }
     //}
-    return result;
 }
 
 int main() {
-    int l = 4; //4, 8, 16, 32, 64
+    // 開始計時
+    auto sw = chrono::high_resolution_clock::now();
+    int l = 16; //4, 8, 16, 32, 64
 
-    //string filename = "dna_500M.txt"; // 檔案名稱
-    //ifstream infile(filename);       // 以讀取模式打開檔案
+    string filename = "dna_1G.txt"; // 檔案名稱
+    ifstream infile(filename);       // 以讀取模式打開檔案
 
-    //// 檢查檔案是否成功打開
-    //if (!infile) {
-    //    cerr << "無法打開檔案 " << filename << endl;
-    //    return 1;
-    //}
+    // 檢查檔案是否成功打開
+    if (!infile) {
+        cerr << "無法打開檔案 " << filename << endl;
+        return 1;
+    }
 
-    //stringstream buffer;
-    //buffer << infile.rdbuf();  // 將檔案內容讀入到 stringstream 中
+    stringstream buffer;
+    buffer << infile.rdbuf();  // 將檔案內容讀入到 stringstream 中
 
-    //string T = buffer.str();  // 將 stringstream 中的內容轉換為 string
-    //string atcgStr2 = T;
+    string T = buffer.str();  // 將 stringstream 中的內容轉換為 string
+    string atcgStr2 = T;
 
-    string T = "GAGTCAGAGTA";
+    //string T = "GAGTCAGAGTA";
     int Tsize = T.length();
     double at = 0;
     int distance = 0;
-    std::vector<int> result;
+    int ans_sizes = Tsize - l + 1;
+    int *result=new int [ans_sizes];
+    
+    // 結束計時
+    auto ew = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> dur = ew - sw;
+    double read= dur.count();
     for (int j = 0; j < 1; j++) {
         // 開始計時
         auto start = chrono::high_resolution_clock::now();
         // 計算漢明距離
-        result = SlidingWindow(T,l);
+        SlidingWindow(T, result,l);
         // 結束計時
         auto end = chrono::high_resolution_clock::now();
         // 計算經過的時間
@@ -73,6 +80,20 @@ int main() {
         /*cout << "漢明距離: " << distance << endl;
         cout << duration.count() << endl;*/
         at += duration.count();
+
+        //std::ofstream outFile("output.txt");
+        //// 檢查文件是否成功打開
+        //if (outFile.is_open()) {
+        //    // 將陣列的每個元素寫入文件，並用空格分隔
+        //    for (int i = 0; i < ans_sizes; ++i) {
+        //        outFile << result[i];
+        //    }
+        //    outFile.close(); // 完成後關閉文件
+        //    std::cout << "陣列已成功寫入 output.txt 文件！" << std::endl;
+        //}
+        //else {
+        //    std::cout << "無法打開文件。" << std::endl;
+        //}
 
     }
     //cout << " T : " << T << endl;
@@ -95,11 +116,12 @@ int main() {
     //    }
     //}
 
-    for (int i = 0; i < result.size(); ++i) {// one substring
-        cout << result[i] << " ";
-    }
+    //for (int i = 0; i < ans_sizes; ++i) {// one substring
+    //    cout << result[i] << " ";
+    //}
 
     cout << endl;
-    cout << "長度為 " << T.length()<< " L=" << l<< " time : " << at << endl;
+    cout <<" L=" << l<< "Distance time : " << at << endl;
+    cout <<" L=" << l<< "readfile time : " << read << endl;
     return 0;
 }
